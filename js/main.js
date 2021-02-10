3282143775dae1b6b97e4ae8c4769af8a68ec6db
Oleg Masyagin <olegmasyagin@gmail.com>
2021-02-10 08:14:07 +0300

    Исправил именование констант

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const getRandomNumber = function(min, max) {
   if(min >= 0 && max > min){
     return Math.floor(Math.random() * ((max-min) + 1) + min);
   }
-  throw ('Ошибка ввода данных');
+  throw new Error ('Ошибка ввода данных');
 }
 
 //Нахождение случайного дробного числа с указанной точностью знаков после запятой.
@@ -15,69 +15,69 @@ const getRandomFloatPoint = function(min, max, fix) {
   if(min >= 0 && max > min) {
     return (Math.random() * (max - min) + min).toFixed(fix);
   }
-  throw ('Ошибка ввода данных');
+  throw new Error ('Ошибка ввода данных');
 }
 
 // getRandomNumber(0, 10);
 // getRandomFloatPoint(1, 7, 2);
 
-const amountObjects = 10;
+const AMOUNT_OBJECTS = 10;
 
-const amountAvatars = [1, 2, 3, 4, 5, 6, 7, 8];
-const titleOffer = ['Квартира на окраине', 'Дворец в центре', 'Дом в частном секторе', 'Бунгало на берегу озера'];
-const priceMin = 0;
-const priceMax = 10000;
-const propertyType = ['palace', 'flat', 'house', 'bungalow'];
-const amountRoomsMin = 1;
-const amountRoomsMax = 5;
-const quantityGuestsMin = 1;
-const quantityGuestsMax = 10;
-const checkinTime = ['12:00', '13:00', '14:00'];
-const checkoutTime = ['12:00', '13:00', '14:00'];
-const featuresOffer = ['wifi', 'dishwasher', 'parking','washer','elevator', 'conditioner'];
-const descriptionOffer = ['Тишина и покой для желающих отдохнуть', 'Красота и роскошь', 'Лучшее место для отдыха', 'Вдали от суеты'];
-const photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-const latitudeMinX = 35.65000;
-const latitudeMaxX = 35.70000;
-const longitudeMinY = 139.70000;
-const longitudeMaxY = 139.80000;
+const AMOUNT_AVATARS = [1, 2, 3, 4, 5, 6, 7, 8];
+const TITLE_OFFER = ['Квартира на окраине', 'Дворец в центре', 'Дом в частном секторе', 'Бунгало на берегу озера'];
+const PRICE_MIN = 0;
+const PRICE_MAX = 10000;
+const PROPERTY_TYPE = ['palace', 'flat', 'house', 'bungalow'];
+const AMOUNT_ROOMS_MIN = 1;
+const AMOUNT_ROOMS_MAX = 5;
+const QUANTITY_GUESTS_MIN = 1;
+const QUANTITY_GUESTS_MAX = 10;
+const CHECKIN_TIME = ['12:00', '13:00', '14:00'];
+const CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
+const FEATURES_OFFER = ['wifi', 'dishwasher', 'parking','washer','elevator', 'conditioner'];
+const DESCRIPTION_OFFER = ['Тишина и покой для желающих отдохнуть', 'Красота и роскошь', 'Лучшее место для отдыха', 'Вдали от суеты'];
+const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const LATITUDE_MIN_X = 35.65000;
+const LATITUDE_MAX_X = 35.70000;
+const LONGITUDE_MIN_Y = 139.70000;
+const LONGITUDE_MAX_Y = 139.80000;
 
 // Функция нахождения случайного элемента в массиве
 
 const getRandomArrElement = function (array) {
-  let element = array[getRandomNumber(0, array.length -1)];
+  const element = array[getRandomNumber(0, array.length -1)];
   return element;
 }
 
 //Генерация массива случайной длины
 
 const getRandomАrr = function (array) {
-  let index = getRandomNumber(0, array.length -1);
-  let newArr = array.slice(index, index + getRandomNumber(1, array.length -1));
+  const index = getRandomNumber(0, array.length -1);
+  const newArr = array.slice(index, index + getRandomNumber(1, array.length -1));
   return newArr;
 }
 
 
 const createAd = function () {
-  let locationX = (getRandomFloatPoint(latitudeMinX, latitudeMaxX, 5));
-  let locationY = (getRandomFloatPoint(longitudeMinY, longitudeMaxY, 5));
+  const locationX = (getRandomFloatPoint(LATITUDE_MIN_X, LATITUDE_MAX_X, 5));
+  const locationY = (getRandomFloatPoint(LONGITUDE_MIN_Y, LONGITUDE_MAX_Y, 5));
   return {
     author: {
-      avatar: 'img/avatars/user0' + getRandomArrElement(amountAvatars) + '.png',
+      avatar: 'img/avatars/user0' + getRandomArrElement(AMOUNT_AVATARS) + '.png',
     },
 
     offer: {
-      title: getRandomArrElement(titleOffer),
+      title: getRandomArrElement(TITLE_OFFER),
       address: locationX + ', ' + locationY,
-      price: getRandomNumber(priceMin, priceMax),
-      type: getRandomArrElement(propertyType),
-      rooms: getRandomNumber(amountRoomsMin, amountRoomsMax),
-      quests: getRandomNumber(quantityGuestsMin, quantityGuestsMax),
-      checkin: getRandomArrElement(checkinTime),
-      checkout: getRandomArrElement(checkoutTime),
-      features: getRandomАrr(featuresOffer),
-      description: getRandomArrElement(descriptionOffer),
-      photos: getRandomАrr(photos),
+      price: getRandomNumber(PRICE_MIN, PRICE_MAX),
+      type: getRandomArrElement(PROPERTY_TYPE),
+      rooms: getRandomNumber(AMOUNT_ROOMS_MIN, AMOUNT_ROOMS_MAX),
+      quests: getRandomNumber(QUANTITY_GUESTS_MIN, QUANTITY_GUESTS_MAX),
+      checkin: getRandomArrElement(CHECKIN_TIME),
+      checkout: getRandomArrElement(CHECKOUT_TIME),
+      features: getRandomАrr(FEATURES_OFFER),
+      description: getRandomArrElement(DESCRIPTION_OFFER),
+      photos: getRandomАrr(PHOTOS),
     },
 
     location: {
@@ -88,8 +88,8 @@ const createAd = function () {
 };
 
 const createArrayAds = function () {
-  let arrAds = [];
-  for (let i = 0; i < amountObjects; i++) {
+  const arrAds = [];
+  for (let i = 0; i < AMOUNT_OBJECTS; i++) {
     arrAds.push(createAd());
   }
   return arrAds;
